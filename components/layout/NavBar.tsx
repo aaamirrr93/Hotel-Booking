9674dbe446275e01cdc9dff40db98c192ce19650
Amir Hossein <87753208+aaamirrr93@users.noreply.github.com>
2024-12-01 19:59:37 +0330

    Create Project

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useAuth, UserButton } from "@clerk/nextjs";
+import React from "react";
+import Container from "../container";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import SearchInput from "../search-input";
+
+function NavBar() {
+  const router = useRouter();
+  const { userId } = useAuth();
+
+  return (
+    <div className="sticky top-0 border border-b-primary/10 bg-secondary">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div
+            className="flex items-center gap-1 cursor-pointer"
+            onClick={() => router.push("/")}
+          >
+            <Image src="/logo.png" width="30" height="30" alt="logo" />
+            <div className="font-extrabold text-xl">StaySavvy</div>
+          </div>
+          <SearchInput />
+          <div className=" flex gap-3 items-center">
+            <div className="">theme</div>
+            <UserButton afterSignOutUrl="/" />
+            {!userId && (
+              <>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  onClick={() => router.push("/sign-in")}
+                >
+                  Sign in
+                </Button>
+                <Button size="sm" onClick={() => router.push("/sign-out")}>
+                  Sign out
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default NavBar;
