513cbba9134c6c0973b017ffd182eb7031512ca3
Amir Hossein <87753208+aaamirrr93@users.noreply.github.com>
2024-12-20 13:55:46 +0330

    add section select location

--- app/hotel/[hotelId]/page.tsx
+++ app/hotel/[hotelId]/page.tsx
@@ -1,15 +1,43 @@
+// import { getHotelById } from "@/actions/getHotelById";
+// import AddHotelForm from "@/components/hotel/AddHotelForm";
+// import { auth } from "@clerk/nextjs/server";
+
+// interface HotelPageProps {
+//   params: {
+//     hotelId: string;
+//   };
+// }
+
+// const Hotel = async ({ params }: HotelPageProps) => {
+//   const hotelId = await params.hotelId;
+//   const hotel = await getHotelById(hotelId);
+//   // const hotel = await getHotelById(params.hotelId);
+//   const { userId } = await auth();
+
+//   if (!userId) return <div>not authenticated ....</div>;
+//   if (hotel && hotel.userId !== userId) return <div>access denied ....</div>;
+//   return (
+//     <div>
+//       <AddHotelForm hotel={hotel} />
+//     </div>
+//   );
+// };
+
+// export default Hotel;
+
 import { getHotelById } from "@/actions/getHotelById";
 import AddHotelForm from "@/components/hotel/AddHotelForm";
 import { auth } from "@clerk/nextjs/server";
 
 interface HotelPageProps {
-  params: {
+  params: Promise<{
     hotelId: string;
-  };
+  }>;
 }
 
 const Hotel = async ({ params }: HotelPageProps) => {
-  const hotel = await getHotelById(params.hotelId);
+  const resolvedParams = await params;
+  const hotel = await getHotelById(resolvedParams.hotelId);
   const { userId } = await auth();
 
   if (!userId) return <div>not authenticated ....</div>;
--- components/hotel/AddHotelForm.tsx
+++ components/hotel/AddHotelForm.tsx
@@ -15,13 +15,22 @@ import {
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 import { Checkbox } from "../ui/checkbox";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { UploadButton } from "../uploadthing";
 import { useToast } from "@/hooks/use-toast";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { Loader2, XCircle } from "lucide-react";
 import axios from "axios";
+import useLocation from "@/hooks/useLocation";
+import { ICity, IState } from "country-state-city";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface AddHotelFormProps {
   hotel: HotelWithRooms | null;
@@ -61,7 +70,13 @@ const formSchema = z.object({
 const AddHotelForm = ({ hotel }: AddHotelFormProps) => {
   const [image, setImage] = useState<string | undefined>(hotel?.image);
   const [imageIsDeleting, setImageIsDeleting] = useState(false);
+  const [states, setStates] = useState<IState[]>([]);
+  const [cities, setCities] = useState<ICity[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const { toast } = useToast();
+  const { getAllCountries, getCountryStates, getStateCities } = useLocation();
+  const countries = getAllCountries();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -86,6 +101,23 @@ const AddHotelForm = ({ hotel }: AddHotelFormProps) => {
       coffeeShop: false,
     },
   });
+
+  useEffect(() => {
+    const selectedCountry = form.watch("country");
+    const countryState = getCountryStates(selectedCountry);
+    if (countryState) {
+      setStates(countryState);
+    }
+  }, [form.watch("country")]);
+
+  useEffect(() => {
+    const selectedCountry = form.watch("country");
+    const selectedState = form.watch("state");
+    const stateCities = getStateCities(selectedCountry, selectedState);
+    if (stateCities) {
+      setCities(stateCities);
+    }
+  }, [form.watch("country"), form.watch("state")]);
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
@@ -354,56 +386,176 @@ const AddHotelForm = ({ hotel }: AddHotelFormProps) => {
                     </FormDescription>
                     <FormControl>
                       {image ? (
-                        <>
-                          <div className="relative max-w-[400px] min-h-[200px] mt-4">
-                            <Image
-                              fill
-                              src={image}
-                              alt="Hotel Image"
-                              className="object-contain"
-                            />
-                            <Button
-                              onClick={() => handleImageDelete(image)}
-                              type="button"
-                              size="icon"
-                              variant="ghost"
-                              className="absolute right-[-12px] top-0"
-                            >
-                              {imageIsDeleting ? <Loader2 /> : <XCircle />}
-                            </Button>
-                          </div>
-                        </>
+                        <div className="relative max-w-[400px] min-h-[200px] mt-4">
+                          <Image
+                            fill
+                            src={image}
+                            alt="Hotel Image"
+                            className="object-contain"
+                          />
+                          <Button
+                            onClick={() => handleImageDelete(image)}
+                            type="button"
+                            size="icon"
+                            variant="ghost"
+                            className="absolute right-[-12px] top-0"
+                          >
+                            {imageIsDeleting ? <Loader2 /> : <XCircle />}
+                          </Button>
+                        </div>
                       ) : (
-                        <>
-                          <div className="flex flex-col items-center max-w-[400px] p-12 border-2 border-dashed border-primary/50 rounded mt-4">
-                            <UploadButton
-                              endpoint="imageUploader"
-                              onClientUploadComplete={(res) => {
-                                // Do something with the response
-                                console.log("Files: ", res);
-                                setImage(res[0].url);
-                                toast({
-                                  variant: "success",
-                                  description: "🎉 Upload Complete",
-                                });
-                              }}
-                              onUploadError={(error: Error) => {
-                                // Do something with the error.
-                                toast({
-                                  variant: "destructive",
-                                  description: `Error! ${error.message}`,
-                                });
-                              }}
-                            />
-                          </div>
-                        </>
+                        <div className="flex flex-col items-center max-w-[400px] p-12 border-2 border-dashed border-primary/50 rounded mt-4">
+                          <UploadButton
+                            endpoint="imageUploader"
+                            onClientUploadComplete={(res) => {
+                              // Do something with the response
+                              console.log("Files: ", res);
+                              setImage(res[0].url);
+                              toast({
+                                variant: "success",
+                                description: "🎉 Upload Complete",
+                              });
+                            }}
+                            onUploadError={(error: Error) => {
+                              // Do something with the error.
+                              toast({
+                                variant: "destructive",
+                                description: `Error! ${error.message}`,
+                              });
+                            }}
+                          />
+                        </div>
                       )}
                     </FormControl>
                   </FormItem>
                 )}
               />
             </div>
-            <div className="">part 2</div>
+            <div className="flex flex-1 flex-col gap-6">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <FormField
+                  control={form.control}
+                  name="country"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>selec Country *</FormLabel>
+                      <FormDescription>
+                        In which country is your property located?
+                      </FormDescription>
+                      <Select
+                        disabled={isLoading}
+                        onValueChange={field.onChange}
+                        value={field.value}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger className="bg-background">
+                          <SelectValue
+                            placeholder="Select a Country"
+                            defaultValue={field.value}
+                          />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {countries.map((country) => (
+                            <SelectItem
+                              key={country.isoCode}
+                              value={country.isoCode}
+                            >
+                              {country.name}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="state"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>select State</FormLabel>
+                      <FormDescription>
+                        In which state is your property located?
+                      </FormDescription>
+                      <Select
+                        disabled={isLoading || states.length < 1}
+                        onValueChange={field.onChange}
+                        value={field.value}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger className="bg-background">
+                          <SelectValue
+                            placeholder="Select state"
+                            defaultValue={field.value}
+                          />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {states.map((state) => (
+                            <SelectItem
+                              key={state.isoCode}
+                              value={state.isoCode}
+                            >
+                              {state.name}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                control={form.control}
+                name="city"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Select City</FormLabel>
+                    <FormDescription>
+                      In which city is your property located?
+                    </FormDescription>
+                    <Select
+                      disabled={isLoading || cities.length < 1}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger className="bg-background">
+                        <SelectValue
+                          placeholder="Select a City"
+                          defaultValue={field.value}
+                        />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {cities.map((city) => (
+                          <SelectItem key={city.name} value={city.name}>
+                            {city.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="locationDescription"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Location Description *</FormLabel>
+                    <FormDescription>
+                      Provide a detailed location description of your hotel
+                    </FormDescription>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Located at the very end of the beach road!"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
           </div>
         </form>
       </Form>
