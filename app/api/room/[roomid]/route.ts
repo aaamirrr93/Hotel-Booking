c8741cad5017fc29c144c6f05764c17177c31673
Amir Hossein <87753208+aaamirrr93@users.noreply.github.com>
2025-03-05 18:15:17 +0330

    fix roomId name folder

--- app/api/room/[roomid]/route.ts
+++ app/api/room/[roomid]/route.ts
@@ -45,13 +45,13 @@ export async function DELETE(
     if (!userId) {
       return new NextResponse("UnAuthorized", { status: 401 });
     }
-    const room = await prismadb.room.delete({
+    const hotel = await prismadb.room.delete({
       where: {
         id: params.roomId,
       },
     });
 
-    return NextResponse.json(room);
+    return NextResponse.json(hotel);
   } catch (error) {
     console.log("Error at /api/room/roomId DELETE", error);
     return new NextResponse("Internal Server Error", { status: 500 });
